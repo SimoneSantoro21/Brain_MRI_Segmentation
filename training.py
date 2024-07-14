d4ce6b9c19c0aaf006dd57300bf55b3beb427bcf
SimoneSantoro21 <simosant2112@gmail.com>
2024-07-14 10:30:39 +0200

    Fixed bug: loss function failed to decrease below 1
    This commit aims to fix the issue with the computation of the loss function at the end of each training epoch.
    Issue was due some missung brackets in lines 69 and 83 of the training script introduced in commit ed305e0.

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import wandb
 
 from libs.dataset import FLAIRDataset
@@ -7,29 +8,36 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader, random_split
 from libs.focal import FocalLoss
 
+
+
 wandb.init(
     # set the wandb project where this run will be logged
     project="bayesian_unet",
 
     # track hyperparameters and run metadata
     config={
-    "learning_rate": 5e-4,
+    "learning_rate": 1e-4,
     "architecture": "Convolutional U-Net",
+    "Dropout": 0.1,
     "dataset": "FLAIR dataset",
     "epochs": 100,
-    "batch size": 4,
-    "Loss function": "FocalLoss",
-    "Alpha": 0.15
+    "batch size": 8,
+    "Loss function": "Focal",
+    "Alpha": 0.25,
+    "gamma": 2,
+    "Reduction": "Mean",
+    "Optimizer": "AdamW",
+    "Layers": 4
     }
 )
 
 
 def train():
-    LEARNING_RATE = 5e-4
-    BATCH_SIZE = 4
+    LEARNING_RATE = 1e-4
+    BATCH_SIZE = 8
     EPOCHS = 100
     DATA_PATH = "org_data"
-    MODEL_SAVE_PATH = "C:/Users/User/Brain_MRI_Segmentation/model/unet.pth"
+    MODEL_SAVE_PATH = "C:/Users/User/Brain_MRI_Segmentation/model/unet_11.pth"
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     dataset = FLAIRDataset(DATA_PATH)
@@ -42,7 +50,7 @@ def train():
 
     model = UNet(in_channels=1, num_classes=1).to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
-    loss_function = FocalLoss(alpha=0.15, gamma=2, reduction="mean")
+    loss_function = FocalLoss(alpha=0.25, gamma=2, reduction="mean")
 
     for epoch in tqdm(range(EPOCHS)):
 
@@ -60,14 +68,10 @@ def train():
             train_running_loss += loss.item()
 
             loss.backward()
-            #for param in model.parameters():
-            #    if torch.any(param.grad != 0):
-            #        print(param.grad)
-            #    break
             optimizer.step()
-    
-        train_loss = train_running_loss / index + 1
-        print("Train loss = ",train_loss)
+
+        train_loss = train_running_loss / (index + 1)
+        print("Train loss = ", train_loss)
 
         model.eval()
         val_running_loss = 0
@@ -80,10 +84,11 @@ def train():
                 loss = loss_function(y_pred, mask)
             
                 val_running_loss += loss.item()
-            val_loss = val_running_loss / index + 1
+            val_loss = val_running_loss / (index + 1)
             print("Validation loss = ", val_loss)
 
         wandb.log({"Training loss": train_loss, "Validation loss:": val_loss})
+
     torch.save(model.state_dict(), MODEL_SAVE_PATH)
 
     return None
