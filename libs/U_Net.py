aa5693400c4f274352a0af561121befcb0b8622a
SimoneSantoro21 <simosant2112@gmail.com>
2024-07-15 18:29:07 +0200

    Added an encoder and decoder block

--- libs/U_Net.py
+++ libs/U_Net.py
@@ -27,15 +27,17 @@ class UNet(nn.Module):
         self.down_convolution_2 = DownSample(64, 128)
         self.down_convolution_3 = DownSample(128, 256)
         self.down_convolution_4 = DownSample(256, 512)
+        self.down_convolution_5 = DownSample(512, 1024)
 
         #Bottleneck
-        self.bottle_neck = DoubleConvolution(512, 1024)
+        self.bottle_neck = DoubleConvolution(1024, 2048)
 
         #Decoder component
-        self.up_convolution_1 = UpSample(1024, 512)
-        self.up_convolution_2 = UpSample(512, 256)
-        self.up_convolution_3 = UpSample(256, 128)
-        self.up_convolution_4 = UpSample(128, 64)
+        self.up_convolution_1 = UpSample(2048, 1024)
+        self.up_convolution_2 = UpSample(1024, 512)
+        self.up_convolution_3 = UpSample(512, 256)
+        self.up_convolution_4 = UpSample(256, 128)
+        self.up_convolution_5 = UpSample(128, 64)
 
         #Output layer
         self.out = nn.Conv2d(in_channels = 64, out_channels = num_classes, kernel_size = 1)
@@ -55,15 +57,17 @@ class UNet(nn.Module):
         convolved_2, pooled_2 = self.down_convolution_2(pooled_1)
         convolved_3, pooled_3 = self.down_convolution_3(pooled_2)
         convolved_4, pooled_4 = self.down_convolution_4(pooled_3)
+        convolved_5, pooled_5 = self.down_convolution_5(pooled_4)
         
-        b_neck = self.bottle_neck(pooled_4)
+        b_neck = self.bottle_neck(pooled_5)
 
-        upsampled_1 = self.up_convolution_1(b_neck, convolved_4)
-        upsampled_2 = self.up_convolution_2(upsampled_1, convolved_3)
-        upsampled_3 = self.up_convolution_3(upsampled_2, convolved_2)
-        upsampled_4 = self.up_convolution_4(upsampled_3, convolved_1)
+        upsampled_1 = self.up_convolution_1(b_neck, convolved_5)
+        upsampled_2 = self.up_convolution_2(upsampled_1, convolved_4)
+        upsampled_3 = self.up_convolution_3(upsampled_2, convolved_3)
+        upsampled_4 = self.up_convolution_4(upsampled_3, convolved_2)
+        upsampled_5 = self.up_convolution_5(upsampled_4, convolved_1)
 
-        output = self.out(upsampled_4)
+        output = self.out(upsampled_5)
         output = nn.Sigmoid()(output)
         
         return output
