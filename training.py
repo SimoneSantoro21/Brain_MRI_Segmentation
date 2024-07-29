93adba6e9388559deccc627e227f0fcc0692bd8d
SimoneSantoro21 <simosant2112@gmail.com>
2024-07-29 15:48:21 +0200

    Adjusted progression bar visualization

--- training.py
+++ training.py
@@ -17,7 +17,7 @@ wandb.init(
 
     # track hyperparameters and run metadata
     config={
-    "learning_rate": 1e-4,
+    "learning_rate": 5e-3,
     "architecture": "Convolutional U-Net",
     "Dropout": 0.1,
     "dataset": "FLAIR dataset",
@@ -34,14 +34,14 @@ wandb.init(
 
 
 def train():
-    LEARNING_RATE = 1e-4
+    LEARNING_RATE = 5e-3
     BATCH_SIZE = 4
-    EPOCHS = 5
+    EPOCHS = 100
     DATA_PATH = "dataset"
     TRAINING_PATH = os.path.join(DATA_PATH, "training")
     VALIDATION_PATH = os.path.join(DATA_PATH, "validation")
 
-    MODEL_SAVE_PATH = "model/prova.pth"
+    MODEL_SAVE_PATH = "model/unet.pth"
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     train_dataset = FLAIRDataset(TRAINING_PATH)
@@ -54,42 +54,49 @@ def train():
     optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
     loss_function = FocalLoss(alpha=0.25, gamma=2, reduction="mean")
 
-    for epoch in tqdm(range(EPOCHS)):
+    for epoch in range(EPOCHS):
+        print(f"{'-' * 50}\nEpoch {epoch+1}/{EPOCHS}")
 
         model.train()
         train_running_loss = 0
 
-        for index, (img, mask) in enumerate(tqdm(train_dataloader)):
-            img = img.float().to(device)
-            mask = mask.float().to(device)
-            
-            prediction = model(img)
-            optimizer.zero_grad()
-
-            loss = loss_function(prediction, mask)
-            train_running_loss += loss.item()
-
-            loss.backward()
-            optimizer.step()
-
-        train_loss = train_running_loss / (index + 1)
-        print("Train loss = ", train_loss)
-
-        model.eval()
-        val_running_loss = 0
-        with torch.no_grad():
-            for index, (img, mask) in enumerate(tqdm(val_dataloader)):
+        with tqdm(total=len(train_dataloader), desc=f"Training Epoch {epoch+1}/{EPOCHS}", leave=False) as pbar:
+            for index, (img, mask) in enumerate(train_dataloader):
                 img = img.float().to(device)
                 mask = mask.float().to(device)
 
-                y_pred = model(img)
-                loss = loss_function(y_pred, mask)
-            
-                val_running_loss += loss.item()
-            val_loss = val_running_loss / (index + 1)
-            print("Validation loss = ", val_loss)
+                prediction = model(img)
+                optimizer.zero_grad()
 
-        wandb.log({"Training loss": train_loss, "Validation loss:": val_loss})
+                loss = loss_function(prediction, mask)
+                train_running_loss += loss.item()
+
+                loss.backward()
+                optimizer.step()
+
+                pbar.update(1)
+
+        train_loss = train_running_loss / (index + 1)
+        print(f"Train loss: {train_loss:.4f}")
+
+        model.eval()
+        val_running_loss = 0
+
+        with tqdm(total=len(val_dataloader), desc=f"Validation Epoch {epoch+1}/{EPOCHS}", leave=False) as pbar:
+            with torch.no_grad():
+                for index, (img, mask) in enumerate(val_dataloader):
+                    img = img.float().to(device)
+                    mask = mask.float().to(device)
+
+                    y_pred = model(img)
+                    loss = loss_function(y_pred, mask)
+
+                    val_running_loss += loss.item()
+                    pbar.update(1)
+        val_loss = val_running_loss / (index + 1)
+        print(f"Validation loss: {val_loss:.4f}")
+
+        wandb.log({"Training loss": train_loss, "Validation loss": val_loss})
 
     torch.save(model.state_dict(), MODEL_SAVE_PATH)
 
